7627dcd331b9eae9e8ab0a4fc56af6faf48e852a
r0swell <r0swell@msn.com>
2016-12-05 20:36:18 +0100

    Add files via upload
    Make ebon crystals drop server wide of any mob with a 1% chance

---
+++ global/global_npc.lua
@@ -0,0 +1,9 @@
+-- Give any NPC in game a chance to drop a Nagafen's token
+function event_death(e)
+	if (math.random(100) <= 99) then
+		e.self:AddItem(40903, 1);
+		e.other:Message(4, "Oh my! A powerfull token dropped");
+	end
+end	
+
+	
