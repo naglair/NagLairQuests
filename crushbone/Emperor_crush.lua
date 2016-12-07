f4fc2a0e95406fbb5fbe4403c52490e242191e97
r0swell <r0swell@msn.com>
2016-12-07 19:12:27 +0100

    Add files via upload
    Added lootableds .. castle bosses share same item tables, other 5 share aug loottable

---
+++ crushbone/Emperor_crush.lua
@@ -0,0 +1,9 @@
+function event_death(e)
+	if (eq.get_zone_instance_version() == 1) then 
+		local itemQuantity = 2;
+		local lootTable = { "68719", "68712", "68715", "68722", "68706", "68714", "68704", "68710", "68703", "68708", "68717", "68720", "68701", "68711" };
+		for i=1, itemQuantity do
+			e.self:AddItem(tonumber(lootTable[math.random(#lootTable)]), 1);
+		end
+	end
+end
