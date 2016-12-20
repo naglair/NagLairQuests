514bb582b097da6c27cb4bc3dbe1603a191767db
r0swell <r0swell@msn.com>
2016-12-20 21:43:20 +0100

    Delete global_npc.lua
    global_npc wont work at all with our current binaries using default.lua instead

--- global/global_npc.lua
+++
@@ -1,9 +0,0 @@
--- Give any NPC in game a chance to drop a Nagafen's token
-function event_death(e)
-	if (math.random(100) <= 1) then
-		e.self:AddItem(40903, 1);
-		e.other:Message(4, "Oh my! A powerfull token dropped");
-	end
-end	
-
-	
