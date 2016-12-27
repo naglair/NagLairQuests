1ef87a9fbffb205845451eda25a55939c5761a8e
r0swell <r0swell@msn.com>
2016-12-27 18:30:46 +0100

    Added enc pets

--- global/default.lua
+++ global/default.lua
@@ -13,24 +13,27 @@ function event_spawn(e)
  	local magPets = { 756, 751, 766, 761, 566, 613, 596, 581 };
 	local necPets = { 629, 630, 631, 632, 633 };
 	local bstPets = { 525, 526, 527, 528 };
-
+	local encPets = { 513, 514 };
+	
 	if (e.self:GetNPCTypeID() == magPets[1] or
 	 	e.self:GetNPCTypeID() == magPets[2] or
 	  	e.self:GetNPCTypeID() == magPets[3] or
 	   	e.self:GetNPCTypeID() == magPets[4] or
-	    e.self:GetNPCTypeID() == magPets[5] or
+	    	e.self:GetNPCTypeID() == magPets[5] or
 		e.self:GetNPCTypeID() == magPets[6] or
 		e.self:GetNPCTypeID() == magPets[7] or
 		e.self:GetNPCTypeID() == magPets[8] or
 	 	e.self:GetNPCTypeID() == necPets[1] or
-     	e.self:GetNPCTypeID() == necPets[2] or
+     		e.self:GetNPCTypeID() == necPets[2] or
 	   	e.self:GetNPCTypeID() == necPets[3] or
 		e.self:GetNPCTypeID() == necPets[4] or
 		e.self:GetNPCTypeID() == necPets[5] or
-	    e.self:GetNPCTypeID() == bstPets[1] or
-	    e.self:GetNPCTypeID() == bstPets[2] or
+	    	e.self:GetNPCTypeID() == bstPets[1] or
+	    	e.self:GetNPCTypeID() == bstPets[2] or
 		e.self:GetNPCTypeID() == bstPets[3] or
-		e.self:GetNPCTypeID() == bstPets[4]) then
+		e.self:GetNPCTypeID() == bstPets[4] or
+		e.self:GetNPCTypeID() == encPets[1] or
+		e.self:GetNPCTypeID() == encPets[2]) then
 			e.self:ModifyNPCStat("max_hp", tostring(e.self:GetHP() * 0.5 + e.self:GetHP()));
 			e.self:ModifyNPCStat("ac", tostring(e.self:GetAC() * 0.5 + e.self:GetAC()));
 			e.self:ModifyNPCStat("min_hit", tostring(e.self:GetMinDMG() * 2 + e.self:GetMinDMG()));
