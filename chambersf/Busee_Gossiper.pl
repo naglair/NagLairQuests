e858e3fd68af3e3e59c5ef97f4672ce14fd63be6
naglair <yovebeki@maileme101.com>
2016-12-27 11:34:40 -0600

    add crushbone shout info
    give players notice of new zone that is open.

--- chambersf/Busee_Gossiper.pl
+++ chambersf/Busee_Gossiper.pl
@@ -1,12 +1,14 @@
 sub EVENT_SAY {
 	if ($text=~/Hail/i) {
-		plugin::Whisper ("Like Oh..Mm..Gee, did you hear what Milli and Vanilli said?");
+		#plugin::Whisper ("Like Oh..Mm..Gee, did you hear what Milli and Vanilli said?");
+		plugin::Whisper("Hear ye, Hear ye! A great evil has given the Orcs of Crushbone immeasureable power!");
+		plugin::Whisper("Adventurers of the highest level are needed! Head to their home in Greater Faydark to help!");
 		
 	}
 }
 my $lastId = 0;
 
-#sub EVENT_SPAWN {
+sub EVENT_SPAWN {
 #    #Get last ID
 #    $connect = plugin::LoadMysql();
 #    $query = "SELECT `id` FROM qs_player_speech ORDER BY `id` DESC LIMIT 1";
@@ -16,9 +18,10 @@ my $lastId = 0;
 #        $lastId = $row[0];
 #    }
 #      quest::settimer("discord", 1);
-#}
+	quest::settimer("shoutstuff",600);
+}
 
-#sub EVENT_TIMER {
+sub EVENT_TIMER {
 #      $connect = plugin::LoadMysql();
 #    $query = "SELECT `from`, `message`, `id` FROM qs_player_speech WHERE `id` > ? AND `type` = 5 AND `to` = '!discord' LIMIT 1";
 #    $query_handle = $connect->prepare($query);
@@ -28,4 +31,8 @@ my $lastId = 0;
 #        $lastId = $row[2];
 #    }
 #    return
-#}
+	if ($timer == "shoutstuff") {
+		quest::shout("Hear ye, Hear ye! A great evil has given the Orcs of Crushbone immeasureable power!");
+		quest::shout("Adventurers of the highest level are needed! Head to their home in Greater Faydark to help!");
+	}
+}
