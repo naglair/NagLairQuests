c78ec89af60990b93b979e4c74298443396702a7
r0swell <r0swell@msn.com>
2016-12-28 16:10:50 +0100

    double loot on dvinn

--- crushbone/999554.lua
+++ crushbone/999554.lua
@@ -1,5 +1,5 @@
 function event_death(e)
-	for i=1, 2 do
+	for i=1, 4 do
 		e.self:AddItem(eq.ChooseRandom(68719, 68712, 68715, 68722, 68706, 68714, 68704, 68710, 68703, 68708, 68717, 68720, 68701, 68711), 1);
 	end
 end
